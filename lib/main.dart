a0b9475ec3debac2ff31bf8ed7483364fc7e9074
HP <saurkumar1994@gmail.com>
2025-03-20 21:50:58 +0530

    Image Network Show

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    // Material app start
     MaterialApp(
-      home: Center(
-        child: Text('Hello, World!'),
-      ),
-    )
+      home: Scaffold(
+        backgroundColor: Colors.cyan[200],
+        appBar: AppBar(
+          title: Text('My First App'),
+          backgroundColor: Colors.cyan[900],
+        ),
+      body:Center(
+        child: 
+       Image.network("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg"),
+      
+      ),),
+    ),
   );
 }
