c433f9eb44c056c064603dd66e55b5c4a31f032e
HP <saurkumar1994@gmail.com>
2025-03-20 21:16:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    // Material app start
+    MaterialApp(
+      home: Center(
+        child: Text('Hello, World!'),
+      ),
+    )
+  );
+}
